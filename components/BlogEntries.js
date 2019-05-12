5dd7caef5157d2824ba7368fea445a175e6c5102
Trevor Hunka <trevorhunka@gmail.com>
2019-05-12 13:11:27 -0400

    styling

--- components/BlogEntries.js
+++ components/BlogEntries.js
@@ -1,40 +1,46 @@
 import React from 'react';
 import Link from 'next/link';
 
+// TODO: CHANGE THIS TO BE BASED ON COLUMNS INSTEAD OF TILES.
+
 export default () => (
   <section className="hero is-light is-small">
-    <div className="tile is-ancestor">
-      <div className="tile is-2 is-vertical is-parent">
-        <div className="tile is-parent">
-          <article className="tile is-child box">
-            <p className="title">Next.js</p>
-            <p className="subtitle">Hello world</p>
-            <div className="content">
-              <p className="blog-post-synopsis">
-                For my first blog post, we'll introduce a quickstart guide to
-                getting a Next.js project underway.
-                <p>
-                  <br />
-                  <Link href="/blog/first">
-                    <a className="read-post-link has-text-link"> Read post!</a>
-                  </Link>
+    <div className="hero-body">
+      <div className="container">
+        <div className="tile is-ancestor">
+          <div className="tile is-3 is-parent">
+            <article className="tile is-child box">
+              <p className="specific-blog-title">Next.js, Hello world</p>
+              <div className="content">
+                <p className="blog-post-synopsis">
+                  For my first blog post, we'll introduce a quickstart guide to
+                  getting a Next.js project underway.
+                  <p>
+                    <br />
+                    <Link href="/blog/first">
+                      <a className="read-post-link has-text-link">
+                        {' '}
+                        Read post!
+                      </a>
+                    </Link>
+                  </p>
                 </p>
-              </p>
-            </div>
-          </article>
+              </div>
+            </article>
+          </div>
+          <div class="tile is-parent">
+            <article class="tile is-child box">
+              <p class="title">Foo</p>
+              <p class="subtitle">Bar</p>
+            </article>
+          </div>
+          <div class="tile is-parent">
+            <article class="tile is-child box">
+              <p class="title">Foo</p>
+              <p class="subtitle">Bar</p>
+            </article>
+          </div>
         </div>
-        {/* <div class="tile is-parent">
-          <article class="tile is-child box">
-            <p class="title">Twelve</p>
-            <p class="subtitle">Subtitle</p>
-            <div class="content">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
-                ornare magna eros, eu pellentesque tortor vestibulum ut.
-              </p>
-            </div>
-          </article>
-        </div> */}
       </div>
     </div>
   </section>
